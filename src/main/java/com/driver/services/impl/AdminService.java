6e4c10fec66e511640cd4274357e27bebdaec46e
John Doe <asif123ali456@gmail.com>
2023-03-04 18:02:23 +0530

    Initial Commit

--- src/main/java/com/driver/services/impl/AdminService.java
+++ src/main/java/com/driver/services/impl/AdminService.java
@@ -1,4 +1,4 @@
-package com.driver.services;
+package com.driver.services.impl;
 
 import com.driver.model.Admin;
 import com.driver.model.ServiceProvider;
@@ -7,5 +7,4 @@ public interface AdminService {
     public Admin register(String username, String password);
     public Admin addServiceProvider(int adminId, String providerName) ;
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception;
-
 }
